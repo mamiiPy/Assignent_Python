0cdc42ba15afdce626f6e69e30136766c0453bf3
mamiiPy <108395583+mamiiPy@users.noreply.github.com>
2022-07-11 00:07:33 +0100

    Add files via upload

---
+++ Assign_03.py
@@ -0,0 +1,48 @@
+# ----------------------- Assignemnt_Three ---------------------------
+# Assign_1: Print All Data type of number
+
+# Assign_2: print The Real and Imagination Number
+
+# Assign_3: Conver Integar Number To Float
+
+# Assign_4: Convert Float To Integer Num
+
+# Assign_5: Operator Calc
+
+# ----------------------- End Assignment_Three -----------------------
+
+print('Assign_1: Print Data Type of Number \n')
+
+print(type(10))
+print(type(10.25))
+print(type(1+3j))
+print('#'*30)
+
+
+print('Assign_2: make varibles In Multibles Lines\n')
+Complex = 1+5j
+print(Complex.real)
+print(Complex.imag)
+print('#'*30)
+
+print('Assign_3: Integer number To Float \n')
+Num = 10
+print(f'The Float Number of {Num} Is : {Num:.10f}')
+print('#'*30)
+
+print('Assign_4: Convert Float to Integer \n')
+Fnum = 159.650
+print(type(Fnum))
+Fnum = int(Fnum)
+print(type(Fnum))
+
+print('#'*30)
+
+print('Assign_5: Operator Calc\n')
+
+print(100-115) #-15
+print(50*30) # 1500
+print(21%4) # 1
+print(110//11) # 10
+print(97//20) # 4
+print('#'*30)
