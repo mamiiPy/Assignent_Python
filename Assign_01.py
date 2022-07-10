89350bfd07a6177b55178c47b47ed968149a948a
mamiiPy <108395583+mamiiPy@users.noreply.github.com>
2022-07-10 07:51:39 +0100

    Add files via upload

---
+++ Assign_01.py
@@ -0,0 +1,41 @@
+# ----------------------- Assignemnt_One ---------------------------
+# Assign_1: Creat a new File Python and Make a description
+
+# Assign_2: Make A three varibles Contain Name,Age,Country , and the Data type is String
+
+# Assign_3: Print The Varibles
+
+# Assign_4: Concatenate The Three Varibles the make a printable Message
+
+# Assign_5: Print The Data Type of Thre Varibles
+# ----------------------- End Assignment_one -----------------------
+
+print('Assign_1: Desciption:')
+
+print('1 - This Is My First Assignemt Solotion For Learning Python Programinge')
+
+print('#'*30)
+
+print('Assign_2: make varibles')
+name = '"mohammed"'
+age = '"31"'
+Country = '"DZD"'
+print('#'*30)
+
+print('Assign_3: Print The Varibles')
+print('"name" :' + name)
+print('"Age" :' + age)
+print('"Country" :' + Country)
+print('#'*30)
+
+print('Assign_4: Concatenate')
+print('Hello!, My name Is: ' + name + 'and Iam a ' +
+      age + ' old', 'I lived in '+Country)
+print('#'*30)
+
+print('Assign_5: Data Type')
+
+print(type(name))
+print(type(age))
+print(type(Country))
+print('#'*30)
