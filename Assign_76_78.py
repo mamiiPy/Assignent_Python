89d37bd30116cd5cea2676c8f63b81a95cf261a4
moahammed_abbad <108395583+mamiiPy@users.noreply.github.com>
2023-03-19 21:53:02 +0100

    Add files via upload

---
+++ Assign_76_78.py
@@ -0,0 +1,18 @@
+# # Assignment nunber one
+import random as rd
+
+# print(f'Random number between 10 and 50 is ==> {rd.randint(10,50)}')
+
+# even= rd.randrange(2,10)
+
+# if even%2 == 0:
+#     print(f'Random even number between 2 and 10 is ==> {even}')
+    
+# odd = rd.randrange(1,9)
+# if odd%2 != 0:
+#     print(f"Random Odd number between 1 and 9 ==> {odd}")
+
+# Assignment 02
+
+
+
